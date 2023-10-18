711078971fdf9f38e2b097f5689206239ecbbf77
Bilal Ibrahim Hairab <bilal.hairab@thirdwayv.com>
2023-10-19 02:26:21 +0400

    add data generator code and result

---
+++ src/data/data_generator.ts
@@ -0,0 +1,32 @@
+// For further or later modifications/reference, The used code in https://json-generator.com/ to generate the 1000 transactions is stated below
+[
+  '{{repeat(1000)}}',
+  {
+    objectType: 'TRANSACTION',
+    status: function (tags) {
+      var status_cases = [
+        'APPROVED',
+        'DECLINED',
+        'PENDING',
+        'CANCELLED',
+        'IN_REVIEW',
+      ];
+      return status_cases[tags.integer(0, status_cases.length - 1)];
+    },
+    objectId: '{{objectId()}}',
+    timestamp:
+      '{{date(new Date(2022, 1, 1), new Date(), "YYYY-MM-ddThh:mm:ss")}}',
+    details: {
+      origin: function (tags) {
+        var status_cases = [
+          'MOBILE_APP',
+          'WEB_PORTAL',
+          'IN_PERSON',
+          'ATM_MACHINE',
+          'PHONE_CALL',
+        ];
+        return status_cases[tags.integer(0, status_cases.length - 1)];
+      },
+    },
+  },
+];
